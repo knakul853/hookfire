e959b9ccaab344ed3e7cc55eb29acc1d25e80e46
knakul853 <knakul853@gmail.com>
2026-06-13 10:42:59 +0530

    test(ui): add missing HUD success-path golden test

--- internal/ui/hud_test.go
+++ internal/ui/hud_test.go
@@ -1,11 +1,26 @@
 package ui
 
 import (
+	"bytes"
+	"os"
 	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 var ansiRE = regexp.MustCompile(`\x1b\[[0-9;]*m`)
 
 func stripANSI(s string) string {
 	return ansiRE.ReplaceAllString(s, "")
+}
+
+func TestHUDSuccessGoldenStripped(t *testing.T) {
+	v := sampleView()
+	v.Overrides = 3
+	var buf bytes.Buffer
+	require.NoError(t, RenderHUD(&buf, v))
+	want, err := os.ReadFile("../../testdata/ui/hud_success.txt")
+	require.NoError(t, err)
+	require.Equal(t, string(want), stripANSI(buf.String()))
 }
